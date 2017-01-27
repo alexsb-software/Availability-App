ffc66603a41f12c4802666cde5811f898f07149a
Ahmed Hamdy <ahmedhamdyau@gmail.com>
2017-01-27 20:06:49 +0200

    Add event shift

---
+++ src/app/events/event-shift.ts
@@ -0,0 +1,7 @@
+import { ShortTimeDate } from './short-time-date';
+
+export class EventShift {
+    start: ShortTimeDate = new ShortTimeDate();
+    end: ShortTimeDate = new ShortTimeDate();
+    constructor() { }
+}
