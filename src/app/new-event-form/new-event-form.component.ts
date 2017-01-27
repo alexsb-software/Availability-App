24406e2a57fb7f6428e189e82b4425cef6ce0dd8
Ahmed Hamdy <ahmedhamdyau@gmail.com>
2017-01-27 14:28:02 +0200

    Initial

---
+++ src/app/new-event-form/new-event-form.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { EventShift } from '../event-shift';
+
+@Component({
+  selector: 'app-new-event-form',
+  templateUrl: './new-event-form.component.html',
+  styleUrls: ['./new-event-form.component.css']
+})
+export class NewEventFormComponent implements OnInit {
+
+  model: EventShift = new EventShift();
+
+  constructor() { }
+
+  ngOnInit() {
+    // Fetch any stored event information ?
+  }
+
+  addShift(): void {
+
+  }
+
+}
---
+++ src/app/short-time-date.ts
@@ -0,0 +1,4 @@
+export class ShortTimeDate {
+    hours: number; minutes: number; isAm: boolean
+
+}
