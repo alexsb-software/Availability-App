5db7ce0b902e7918b46f1ed96b829e7e4e5f9983
Ahmed Hamdy <ahmedhamdyau@gmail.com>
2017-01-31 18:48:01 +0200

    Clear custom forms files

--- src/app/custom-forms/control-value-accessor.ts
+++
@@ -1,6 +0,0 @@
-export interface ControlValueAccessor {
-    writeValue(obj:any):void;
-    registerOnChange(fn:any):void;
-    registerOnTouched(fn:any):void;
-    setDisabledState(isDisabled:boolean):void;
-}
