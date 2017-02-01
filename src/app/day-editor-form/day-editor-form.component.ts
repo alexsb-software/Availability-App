1b2e8bb001ae6bd10586945985708243b37e84ba
Ahmed Hamdy <ahmedhamdyau@gmail.com>
2017-02-01 18:10:51 +0200

    Merge event-day to master

---
+++ src/app/day-editor-form/day-editor-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-day-editor-form',
+  templateUrl: './day-editor-form.component.html',
+  styleUrls: ['./day-editor-form.component.css']
+})
+export class DayEditorFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
