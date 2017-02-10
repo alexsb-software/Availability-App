9d1d9a56549c5786053057b653c3c1f9cc2c47e5
Mahmoudsultan <mahmoud.m.sultan@gmail.com>
2017-02-10 14:32:03 +0200

    post new event service

--- src/app/events/eventdata.service.ts
+++ src/app/events/eventdata.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { EventUser } from './event-user';
 import { UserAvalability } from '../user_reg/user';
-
+import {Event} from '../applogic-general/event';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
@@ -10,6 +10,7 @@ import 'rxjs/add/operator/map';
 export class EventDataService {
 
     private _apiurl = '../api/events.json';
+    private newEventAPI = '../api/newevent';
 
     constructor(private _http: Http) { }
 
@@ -43,6 +44,51 @@ export class EventDataService {
             .map((response: Response) => <number>response.status)
     }
 
-    //postEvent()
+    postEvent(event: Event) : Observable<number>{
+        return this._http.post(this.newEventAPI, JSON.stringify(this.parseToJson(event)))
+            .map((response: Response) => <number>response.status)
+    }
+
+    private parseToJson(event: Event) : Object {
+
+        let eventObj = {
+            'eventName': event.eventName,
+            'eventDays': []
+        }
+
+        
+        for (let day of event.eventDays) {
+            
+            let eventDayObj = {
+                'dayDate': day.dayDate,
+                'shifts': []
+            }
+
+            for (let shift of day.shifts) {
+                
+                let shiftObj = {
+                    'number': shift.number,
+                    'startDate': shift.startDate,
+                    'endDate': shift.endDate,
+                    'sessions': []
+                }
+
+                for(let session of shift.sessions) {
+                    let sessionObj = {};
+                    sessionObj['name'] = session.name;
+                    sessionObj['notes'] = session.notes;
+                    sessionObj['reporting'] = session.reporting.id;
+                    sessionObj['pr'] = session.publicRelations.id;            
+                    shiftObj.sessions.push(sessionObj);
+                }
+
+                eventDayObj.shifts.push(shiftObj);
+            }
+
+            eventObj.eventDays.push(eventDayObj);
+        }
+
+        return eventObj;
+    }
 
 }
