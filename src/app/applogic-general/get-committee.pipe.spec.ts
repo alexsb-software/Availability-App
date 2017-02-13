dc08c7a77683cc6ba7dc3bf814dff7592cbb26ab
Ahmed Hamdy <ahmedhamdyau@gmail.com>
2017-02-13 16:22:21 +0200

    Fix disappearing components when no members

---
+++ src/app/applogic-general/get-committee.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { GetCommitteePipe } from './get-committee.pipe';
+
+describe('GetCommitteePipe', () => {
+  it('create an instance', () => {
+    const pipe = new GetCommitteePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
