c8a1e40cb8ac4fc15650a4dc12fea0fca50fccdb
MahmoudSultan <mahmoud.m.sultan36@gmail.com>
2017-02-21 22:39:29 +0200

    Auth_token generator change to be compatible with php5

--- src/api/auth/tokens.php
+++ src/api/auth/tokens.php
@@ -1,7 +1,8 @@
 <?php
 
     function generate_token() {
-        return $token = bin2hex(random_bytes(30));
+        //return $token = bin2hex(random_bytes(30));
+        return bin2hex(openssl_random_pseudo_bytes(30));
     }
 
 ?>
