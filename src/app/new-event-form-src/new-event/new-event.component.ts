04a1478405e2feb033903040569e620767b44bf8
Mahmoudsultan <mahmoud.m.sultan@gmail.com>
2017-02-22 23:54:25 +0200

    Login redirect added

--- src/app/new-event-form-src/new-event/new-event.component.ts
+++ src/app/new-event-form-src/new-event/new-event.component.ts
@@ -3,19 +3,24 @@ import { EventDay } from '../../applogic-general/event-day';
 import { EventDataService } from '../../events/eventdata.service';
 import { Event } from '../../applogic-general/event';
 import {leftFadeInOut,fallIn } from '../../animation/animation'
+import {UserAuthService} from '../../user-auth/user-auth.service';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
 @Component({
   selector: 'app-new-event',
   templateUrl: './new-event.component.html',
   styleUrls: ['./new-event.component.css'],
   animations:[leftFadeInOut(),fallIn()]
 })
-export class NewEventComponent {
+export class NewEventComponent implements OnInit {
   eventName: string;
   dayCount: number = 1;
   event: Event;
   state = 'in';
 
-  constructor(private service: EventDataService) {
+  constructor(private service: EventDataService,
+    private _userauth: UserAuthService,
+    private router: Router) {
     this.event = new Event();
     this.event.eventDays = [];
     //this.event.eventDays.push(new EventDay());
@@ -28,6 +33,12 @@ export class NewEventComponent {
   // by its index that will be added to the function
   // parameters while in the loop
   // The for loo
+
+  ngOnInit() {
+    if (!this._userauth.loggedIn()) {
+        this.router.navigate(['/login'], { queryParams: { returnUrl: '/event/new' }});
+    }
+  }
 
   removeDay(dayIdx: number) {
     this.event.eventDays.splice(dayIdx, 1);
