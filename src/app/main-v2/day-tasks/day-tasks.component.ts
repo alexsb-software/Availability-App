690de8231b1374e00fd86a8cb77f9a1beaebe0ea
Ahmed Hamdy <ahmedhamdyau@gmail.com>
2017-03-01 01:40:12 +0200

    Shift committee members OK

---
+++ src/app/main-v2/day-tasks/day-tasks.component.ts
@@ -0,0 +1,60 @@
+import { Component, Input, Output, OnDestroy, EventEmitter, OnInit } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Rx';
+
+import { MemberHolderService } from '../app-services/member-holder.service';
+import { Filters } from '../logic/filters';
+import { Committee, CommitteeEnum } from '../logic/committee';
+import { Member } from '../logic/member';
+
+
+@Component({
+  selector: 'app-day-tasks',
+  templateUrl: './day-tasks.component.html',
+  styles: []
+})
+export class DayTasksComponent implements OnInit {
+
+  subscription: Subscription;
+  dayIndex: number = -1;
+  members: Member[] = [];
+  /**
+   * Used for ngFor in HTML
+   */
+  iterableShiftCount: number[] = [];
+  constructor(private router: Router,
+    private route: ActivatedRoute,
+    private memberService: MemberHolderService) {
+
+  }
+  ngOnInit() {
+    if (this.memberService.days.length === 0) {
+      this.router.navigate(['/v2']);
+      return; // TODO cancel navigation
+    }
+    this.route.params
+      .switchMap((params) => this.dayIndex = params['id'])
+      .subscribe(e => { });
+
+    this.subscription = this.router.events.subscribe(e => {
+      if (e instanceof NavigationEnd) {
+        this.members = Filters.byDay(this.memberService.members, this.dayIndex);
+
+        let shiftCount = this.memberService.getShiftCount(this.dayIndex);
+        console.debug("Shift count", shiftCount, this.dayIndex);
+
+        this.iterableShiftCount = Array(shiftCount).fill(0);
+      }
+    });
+  }
+
+  getAllCommittees(): string[] {
+    return Committee.getAll();
+  }
+
+  getMembersOfCommittee(shiftIndex: number, commName: string): Member[] {
+    let temp: Member[] = Filters.byShift(this.members, this.dayIndex, shiftIndex);
+    return Filters.byCommittee(temp, commName);
+  }
+
+}
