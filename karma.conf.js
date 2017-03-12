e56c23d1c909107be32bd81db5fa632ab1fd5862
Amr Fathy <amruafathy@gmail.com>
2017-03-12 15:54:33 +0200

    Add base url

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
+    basePath: '/events-app/',
     frameworks: ['jasmine', 'angular-cli'],
     plugins: [
       require('karma-jasmine'),
